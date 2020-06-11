232b17247e8f3db8c0b5425f781687e064cf5324
Ramesh Suragam <ramesh.suragam@gmail.com>
2020-06-11 14:29:28 +0100

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "CNA"
+
+version := "0.1"
+
+scalaVersion := "2.12.11"
+
+val sparkVersion = "2.4.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-graphx" % sparkVersion
+)
---
+++ src/main/scala/citation.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object citation {
+  def main(args: Array[String]): Unit = {
+
+    //creating a case Class for Journal
+//    case class Journal(entities: Array[String], journalVolume: Int, journalPages: String, pmid: String, fieldsOfStudy: Array[String], year: Int, outCitations: Array[String], s2Url: String, s2PdfUrl: String, id: String, authors: Array[String], journalName: String, paperAbstract: String, inCitations: Array[String], pdfUrls: Array[String], title: String, doi: Array[String], sources: Array[String], doiUrl: Array[String], venue: String)
+
+    val conf = new SparkConf().setAppName("citation").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    val textRDD = sc.textFile("file:///All Items Offline/Sem2/CS648 Project/sample_data/s2-corpus-00/s2-corpus-00")
+    println("one")
+    textRDD.take(10).foreach(println)
+    println("two")
+  }
+}
